4638584873cedc908d71ae012be0bd33d6a7e414
AleXwern <alex.nystrom@suunto.com>
2024-11-25 17:43:40 +0200

    Did this in 30 minutes as opposed to 60 minuted to unfinished version.

---
+++ Makefile
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: AleXwern <AleXwern@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/25 17:02:22 by AleXwern          #+#    #+#              #
+#    Updated: 2024/11/25 17:41:59 by AleXwern         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME	= brackets.exe
+FLAG	= -m64 -Wall -Wextra -Werror
+SRCFILE	= main.cpp
+OBJ		= $(addprefix ./obj/,$(SRCFILE:.cpp=.o))
+INCLS	= -I ./includes
+GREEN	= \033[0;32m
+PURPLE	= \033[0;35m
+STOP	= \033[0m
+TEMP0	= 0
+
+.PHONY: all clean fclean re
+
+all: $(NAME)
+
+./obj/%.o:./src/%.cpp
+	@echo "Compiling $(GREEN)$@$(STOP)"
+	@mkdir -p obj
+	g++ $(FLAG) -c $< -o $@ $(INCLS)
+	@cp $@ ../obj
+
+folders:
+	@mkdir -p build
+	@mkdir -p obj
+
+$(NAME): folders $(OBJ)
+	g++ -o $(NAME) $(OBJ)
+
+clean:
+	@/bin/rm -f $(OBJ)
+	@/bin/rm -rf ./obj
+
+fclean: clean
+	@rm -rf $(NAME)
+
+re: fclean all
---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: AleXwern <AleXwern@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 17:05:43 by AleXwern          #+#    #+#             */
+/*   Updated: 2024/11/25 17:36:12 by AleXwern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+std::string	bracketMatcher(const char *src)
+{
+	std::vector<char>	brackets = {0};
+	int					bracketCount = 0;
+
+	while (*src && bracketCount != -1)
+	{
+		switch (*src)
+		{
+		case '(':
+		case '[':
+			brackets.push_back(*src);
+			break;
+		case ']':
+			if (brackets.back() != '[')
+				bracketCount = -1;
+			else
+			{
+				bracketCount++;
+				brackets.pop_back();
+			}
+			break;
+		case ')':
+			if (brackets.back() != '(')
+				bracketCount = -1;
+			else
+			{
+				bracketCount++;
+				brackets.pop_back();
+			}
+			break;
+		}
+		src++;
+	}
+	if (bracketCount > 0)
+		return "1 " + std::to_string(bracketCount);
+	else
+		return "0";
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	if (argc < 2)
+		std::cout << bracketMatcher("") << std::endl;
+	else
+		std::cout << bracketMatcher(argv[1]) << std::endl;
+	return 0;
+}
